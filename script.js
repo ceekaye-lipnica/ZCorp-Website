a4a273d00047c6859a2f57d92bcc0ce39ffe6b21
ceekaye-lipnica <ceekayelipnica04@gmail.com>
2024-03-16 17:50:29 +0800

    03/16/24

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ links.forEach((drop) => {
 		drop.querySelector("i").classList.toggle("open");
 	});
 });
+
+// TOGGLE MENU 
+const toggleMenu = document.querySelector(".toggle__menu");
+const headNav = document.querySelector(".header-mobile-nav");
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("open");
+    headNav.classList.toggle("open");
+});
