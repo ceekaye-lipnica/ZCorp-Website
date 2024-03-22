45205d199ea00780cd5bd3633acc8bec42f69a2c
ceekaye-lipnica <ceekayelipnica04@gmail.com>
2024-03-22 17:28:29 +0800

    03/22/24

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ links.forEach((drop) => {
 	});
 });
 
+const links2 = document.querySelectorAll(".header-nav-dropdown");
+links2.forEach((drop2) => {
+	drop2.addEventListener("click", () => {
+		drop2.nextElementSibling.classList.toggle("open");
+		drop2.querySelector("i").classList.toggle("open");
+	});
+});
+
+const links3 = document.querySelectorAll(".header-mobile-nav-dropdown h4");
+links3.forEach((drop3) => {
+	drop3.addEventListener("click", () => {
+		drop3.nextElementSibling.classList.toggle("open");
+		drop3.querySelector("i").classList.toggle("open");
+	});
+});
+
 // TOGGLE MENU 
 const toggleMenu = document.querySelector(".toggle__menu");
 const headNav = document.querySelector(".header-mobile-nav");
